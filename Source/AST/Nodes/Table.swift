d0f5fb28ad658daf8f481df7886895f09284ff9f
Brandon Asuncion <me@brandonasuncion.dev>
2020-03-09 03:51:57 -0700

    table nodes

--- Source/AST/Nodes/Table.swift
+++ Source/AST/Nodes/Table.swift
@@ -10,8 +10,36 @@ import Foundation
 
 public class Table: BaseNode {
     public private(set) lazy var literal: String? = cmarkNode.literal
-    public private(set) lazy var fenceInfo: String? = cmarkNode.fenceInfo
+    
+    public var columns: Int {
+        cmark_gfm_extensions_get_table_columns(cmarkNode)
+    }
+
+    public var alignments: UInt8 {
+        cmark_gfm_extensions_get_table_alignments(cmarkNode)
+    }
 }
+
+public class TableRow: BaseNode {
+    public private(set) lazy var literal: String? = cmarkNode.literal
+
+    public var isHeader: Bool {
+        cmark_gfm_extensions_get_table_row_is_header(cmarkNode) != 0
+    }
+}
+
+public class TableCell: BaseNode {
+    public private(set) lazy var innerText: String = cmarkNode.literal ?? ""
+    public private(set) lazy var innerLength: Int = literal.count
+    public private(set) lazy var maxWordLen: Int = literal
+        .split(separator: " ")
+        .compactMap { String($0).count }
+        .max() ?? 0
+}
+
+
+
+
 
 extension Table: CustomDebugStringConvertible {
     public var debugDescription: String {
@@ -19,22 +47,10 @@ extension Table: CustomDebugStringConvertible {
     }
 }
 
-
-public class TableRow: BaseNode {
-    public private(set) lazy var literal: String? = cmarkNode.literal
-    public private(set) lazy var fenceInfo: String? = cmarkNode.fenceInfo
-}
-
 extension TableRow: CustomDebugStringConvertible {
     public var debugDescription: String {
         return "TableRow - content: \(literal)"
     }
-}
-
-
-public class TableCell: BaseNode {
-    public private(set) lazy var literal: String? = cmarkNode.literal
-    public private(set) lazy var fenceInfo: String? = cmarkNode.fenceInfo
 }
 
 extension TableCell: CustomDebugStringConvertible {
